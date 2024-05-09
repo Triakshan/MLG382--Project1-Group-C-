4c5bd51d75d5034dac80aa3796eb6146c08aad35
Henry Roux <111967054+HXCLogic@users.noreply.github.com>
2024-05-09 18:35:07 +0200

    We fixed all errors
    Co-Authored-By: AlbertBuys <141368807+AlbertBuys@users.noreply.github.com>

--- MLG382- Project 1- Group C/notebooks/src/preprocess_data.py
+++ MLG382- Project 1- Group C/notebooks/src/preprocess_data.py
@@ -14,16 +14,6 @@ def preprocess(df: pd.DataFrame):
     # Fill numerical values
     df['Credit_History'].fillna(df['Credit_History'].mean(), inplace=True)
 
-    # One Hot Encoding Categorical Columns
-    df['Dependents'] = df['Dependents'].replace(to_replace='3+', value='4')
-
-    df['Gender'] = df['Gender'].map({'Male' : 1, 'Female' : 0}).astype('int')
-    df['Married'] = df['Married'].map({'Yes' : 1, 'No' : 0}).astype('int')
-    df['Education'] = df['Education'].map({'Graduate' : 1, 'Not Graduate' : 0}).astype('int')
-    df['Self_Employed'] = df['Self_Employed'].map({'Yes' : 1, 'No' : 0}).astype('int')
-    df['Property_Area'] = df['Property_Area'].map({'Rural' : 0, 'Urban' : 1, 'Semiurban': 2}).astype('int')
-    df['Loan_Status'] = df['Loan_Status'].map({'Y' : 1, 'N' : 0}).astype('int')
-
     # Create a mask to filter out the outliers for 'ApplicantIncome'
     mask_ApplicantIncome = df['ApplicantIncome'] <= 7441
 
@@ -39,5 +29,8 @@ def preprocess(df: pd.DataFrame):
 
     # Filter out the outliers from the dataframe
     df = df[mask_ApplicantIncome & mask_CoapplicantIncome & mask_LoanAmount1 & mask_LoanAmount2 & mask_Loan_Amount_Term]
+
+    # Scaling
+    df['Loan_Status'] = df['Loan_Status'].map({'Y' : 1, 'N' : 0}).astype('int')
 
     return df
--- MLG382- Project 1- Group C/notebooks/src/train_models.py
+++ MLG382- Project 1- Group C/notebooks/src/train_models.py
@@ -2,13 +2,6 @@ import numpy as np
 import pandas as pd
 
 def model_1(df: pd.DataFrame):
-    # Scale the numerical features
-    cols = ['ApplicantIncome', 'CoapplicantIncome', 'LoanAmount', 'Loan_Amount_Term']
-
-    from sklearn.preprocessing import StandardScaler
-    ss = StandardScaler()
-    df[cols] = ss.fit_transform(df[cols])
-
     # Splitting the dataset into the Training set and Test set
     from sklearn.model_selection import train_test_split
 
@@ -18,59 +11,41 @@ def model_1(df: pd.DataFrame):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
 
     # Model and Predict
-    from sklearn.metrics import accuracy_score
-
-    # Create an instance of the DecisionTreeClassifier
-    from sklearn.tree import DecisionTreeClassifier
-    model_dt = DecisionTreeClassifier()
-
-    # Fit the model to the training data
-    model_dt.fit(X_train, y_train)
-
-    # Evaluate the model on the testing data
-    y_pred = model_dt.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
-    print("Decision Tree Accuracy:", (accuracy*100).__round__(4))
-
-
-    # Create an instance of the Random Forest Classifier
-    from sklearn.ensemble import RandomForestClassifier
-    model_rf = RandomForestClassifier()
-
-    # Fit the model to the training data
-    model_rf.fit(X_train, y_train)
-
-    # Evaluate the model on the testing data
-    y_pred = model_rf.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
-    print("Random Forest Accuracy:", (accuracy*100).__round__(4))
-
-    # Create an instance of the Logistic Regression
+    from sklearn.pipeline import make_pipeline
+    from category_encoders import OneHotEncoder
+    from sklearn.preprocessing import StandardScaler
     from sklearn.linear_model import LogisticRegression
-    model_lr = LogisticRegression()
+    
+    model = make_pipeline(
+        OneHotEncoder(use_cat_names=True), # encode cat features
+        StandardScaler(), # imputation
+        LogisticRegression()) # build model
 
-    # Fit the model to the training data
-    model_lr.fit(X_train, y_train)
+    # fit the model
+    model.fit(X_train, y_train)
 
-    # Evaluate the model on the testing data
-    y_pred = model_lr.predict(X_test)
+    # Accuracy of model
+    from sklearn.metrics import accuracy_score
+    y_pred = model.predict(X_test)
     accuracy = accuracy_score(y_test, y_pred)
-    print("Logistic Regression Accuracy:", (accuracy*100).__round__(4))
+    print("Logistic Regression:", (accuracy*100).__round__(4))
 
-    return model_dt, model_rf, model_lr
+    from sklearn.metrics import mean_absolute_error
+    # Predict the train data
+    y_pred_training = model.predict(X_train)
+    y_pred_test = model.predict(X_test)
+
+    # Compute MAE
+    print("Training MAE:", round(mean_absolute_error(y_train, y_pred_training),2))
+    print("Test data MAE:", round(mean_absolute_error(y_test, y_pred_test),2))
+
+    return model
 
 def model_2(df: pd.DataFrame):
     # Feature Engineering
     df['Income'] = df['ApplicantIncome'] + df['CoapplicantIncome']
     df.drop(columns=['ApplicantIncome', 'CoapplicantIncome'], inplace=True)
-
-    # Scale the numerical features
-    cols = ['Income', 'LoanAmount', 'Loan_Amount_Term']
-
-    from sklearn.preprocessing import StandardScaler
-    ss = StandardScaler()
-    df[cols] = ss.fit_transform(df[cols])
-
+    
     # Splitting the dataset into the Training set and Test set
     from sklearn.model_selection import train_test_split
 
@@ -80,43 +55,32 @@ def model_2(df: pd.DataFrame):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
 
     # Model and Predict
-    from sklearn.metrics import accuracy_score
-
-    # Create an instance of the DecisionTreeClassifier
-    from sklearn.tree import DecisionTreeClassifier
-    model_dt = DecisionTreeClassifier()
-
-    # Fit the model to the training data
-    model_dt.fit(X_train, y_train)
-
-    # Evaluate the model on the testing data
-    y_pred = model_dt.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
-    print("Decision Tree Accuracy:", (accuracy*100).__round__(4))
-
-
-    # Create an instance of the Random Forest Classifier
-    from sklearn.ensemble import RandomForestClassifier
-    model_rf = RandomForestClassifier()
-
-    # Fit the model to the training data
-    model_rf.fit(X_train, y_train)
-
-    # Evaluate the model on the testing data
-    y_pred = model_rf.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
-    print("Random Forest Accuracy:", (accuracy*100).__round__(4))
-
-    # Create an instance of the Logistic Regression
+    from sklearn.pipeline import make_pipeline
+    from category_encoders import OneHotEncoder
+    from sklearn.preprocessing import StandardScaler
     from sklearn.linear_model import LogisticRegression
-    model_lr = LogisticRegression()
+    
+    model = make_pipeline(
+        OneHotEncoder(use_cat_names=True), # encode cat features
+        StandardScaler(), # imputation
+        LogisticRegression()) # build model
 
-    # Fit the model to the training data
-    model_lr.fit(X_train, y_train)
+    # fit the model
+    model.fit(X_train, y_train)
 
-    # Evaluate the model on the testing data
-    y_pred = model_lr.predict(X_test)
+    # Accuracy of model
+    from sklearn.metrics import accuracy_score
+    y_pred = model.predict(X_test)
     accuracy = accuracy_score(y_test, y_pred)
-    print("Logistic Regression Accuracy:", (accuracy*100).__round__(4))
+    print("Logistic Regression:", (accuracy*100).__round__(4))
 
-    return model_dt, model_rf, model_lr
+    from sklearn.metrics import mean_absolute_error
+    # Predict the train data
+    y_pred_training = model.predict(X_train)
+    y_pred_test = model.predict(X_test)
+
+    # Compute MAE
+    print("Training MAE:", round(mean_absolute_error(y_train, y_pred_training),2))
+    print("Test data MAE:", round(mean_absolute_error(y_test, y_pred_test),2))
+
+    return model
